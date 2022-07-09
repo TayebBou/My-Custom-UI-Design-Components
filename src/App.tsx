55263aa55d58388cd8a6e3c9aee7838698c4e0ea
TayebBou <tayebbousfiha72@gmail.com>
2022-07-09 16:48:22 +0200

    Project Initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import styles from './App.module.css';
+
+const App:FC = () => {
+  return (
+    <div className={styles.app}>
+    </div>
+  );
+}
+
+export default App;
