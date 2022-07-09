d632c47bee62bdca3b876b369316d685e5c2682e
TayebBou <tayebbousfiha72@gmail.com>
2022-07-09 17:52:38 +0200

    Add big Image slider molecule

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
 import styles from './App.module.css';
+import BigImageSlider from './components/molecules/ImageSlider/BigImageSlider';
+import data from './server/data.json'
+import { IPhoto } from './shared/types/photo.model';
 
 const App:FC = () => {
   return (
     <div className={styles.app}>
+      <BigImageSlider photos={data.photos as IPhoto[]} />
     </div>
   );
 }
