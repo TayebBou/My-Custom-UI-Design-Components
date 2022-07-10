82e2e77d070fd2c2e623d8f6d7f60c0cb184e654
TayebBou <tayebbousfiha72@gmail.com>
2022-07-10 23:33:12 +0200

    Add button Atom, full screen molecule and bigImageSlider responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { FC } from 'react';
-import styles from './App.module.css';
-import BigImageSlider from './components/molecules/ImageSlider/BigImageSlider';
+import { FC } from 'react'
+import styles from './App.module.css'
+import BigImageSlider from './components/organismes/BigImageSlider/BigImageSlider'
 import data from './server/data.json'
-import { IPhoto } from './shared/types/photo.model';
+import { IPhoto } from './shared/types/photo.model'
 
-const App:FC = () => {
+const App: FC = () => {
+
   return (
     <div className={styles.app}>
       <BigImageSlider photos={data.photos as IPhoto[]} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
